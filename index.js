f131f757d35b7b39b1cb5ba09e1d59b429d243fd
Dashevskii Dmitrii <rus51ddl@gmail.com>
2022-07-26 21:11:49 +0300

    merge feature/user-access in main

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-<<<<<<< HEAD
 console.log('Heloo World')
 
 const userAge = 25
 if (userAge >= 18) {
     console.log('Вы совершеннолетний')
 } else {
-=======
-console.log('Hello World')
-
-const age = 20
-if (age >= 18) {
-    console.log('Вы совершеннолетний')
-}
-else {
->>>>>>> feature/user-access
     console.log('Вы не совершеннолетний')
 }
